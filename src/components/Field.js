f5240015bfcb9fb5659c6fe7cee0e1c77b340ee5
Jinseo Sung <jinseosung15@gmail.com>
2023-11-03 09:44:04 +0100

    add Field component

---
+++ src/components/Field.js
@@ -0,0 +1,53 @@
+import { useState, useEffect, useRef } from "react";
+import bug from "../assets/img/bug.png";
+import carrot from "../assets/img/carrot.png";
+
+const Field = () => {
+  const [bugs, setBugs] = useState([]);
+  const [carrots, setCarrots] = useState([]);
+  const ref = useRef(null);
+
+  useEffect(() => {
+    const fieldRect = ref.current.getBoundingClientRect();
+    const fieldWidth = fieldRect.width;
+    const fieldHeight = fieldRect.height;
+
+    addItems(5, setCarrots, carrot, fieldWidth, fieldHeight);
+    addItems(5, setBugs, bug, fieldWidth, fieldHeight);
+  }, []);
+
+  const randomNum = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  };
+
+  const addItems = (num, setItemsFunc, imgSrc, maxWidth, maxHeight) => {
+    const items = [];
+    for (let i = 0; i < num; i++) {
+      const style = {
+        position: "absolute",
+        left: `${randomNum(0, maxWidth - 50)}px`,
+        bottom: `${randomNum(0, maxHeight - 50)}px`,
+      };
+
+      items.push(
+        <img
+          key={i}
+          src={imgSrc}
+          alt={imgSrc === "bug" ? "bug" : "carrot"}
+          style={style}
+        />
+      );
+    }
+
+    setItemsFunc(items);
+  };
+
+  return (
+    <section ref={ref} className="game__field">
+      {carrots}
+      {bugs}
+    </section>
+  );
+};
+
+export default Field;
